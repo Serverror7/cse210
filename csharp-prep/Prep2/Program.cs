0ebda45141b13af6a3b3478991a174fa8596ddc2
Serverror7 <mar22043@byui.edu>
2025-09-20 21:19:53 -0600

    hwk 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your percentage score? ");
+        string percentage = Console.ReadLine();
+        string letterGrade = "";
+        int number = int.Parse(percentage);
+        if (number >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (number >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (number >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (number >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        Console.WriteLine($"Your grade is {letterGrade}.");
+        if (number >= 70)
+        {
+            Console.Write("You passed the course! :) Congrats!");
+        }
+        else
+        {
+            Console.Write("You did not pass the class. :( Better luck next time!");
+        }
     }
 }
