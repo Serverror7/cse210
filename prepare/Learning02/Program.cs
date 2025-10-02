a4020e4b739291c43fe74efd12b1a72b2d347811
Serverror7 <mar22043@byui.edu>
2025-10-02 14:33:05 -0600

    Prepare 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Reflection;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        
+        job1._jobTitle = "White-hat Hacker";
+        job1._company = "ACME CO.";
+        job1._startYear = 2005;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "Electical Engineer";
+        job2._company = "Department of Energy";
+        job2._startYear = 2009;
+        job2._endYear = 2011;
+
+        Resume myResume = new Resume();
+
+        myResume._name = "Joesph Smith";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
+
